a07bf12a3261c125f1185f06e82e3a7587d0758b
bhupender8448 <113782918+bhupender8448@users.noreply.github.com>
2023-05-19 20:49:49 +0530

    2nd interview

--- reels/firebase.js
+++ reels/firebase.js
@@ -21,75 +21,75 @@
 
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
-import {getStorage} from 'firebase/storage';
-import {getFirestore} from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDZxMugqd8B2a53scfhFldyRGrlyPNAvkU",
-  authDomain: "reels-next-d28a7.firebaseapp.com",
-  projectId: "reels-next-d28a7",
-  storageBucket: "reels-next-d28a7.appspot.com",
-  messagingSenderId: "279478513733",
-  appId: "1:279478513733:web:05bac33a46cbe29e4bf32c"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-
-const auth = getAuth();
-const storage = getStorage();
-const db = getFirestore();
-export { auth,storage,db }
-export default app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+// import {getAuth} from 'firebase/auth';
+// import {getStorage} from 'firebase/storage';
+// import {getFirestore} from 'firebase/firestore';
 // // TODO: Add SDKs for Firebase products that you want to use
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
 // // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // const firebaseConfig = {
-//   apiKey: "AIzaSyBN8rNnQ3gfpa1IfeiUrMVC8W_CxV9xOW4",
-//   authDomain: "ndinterveiw.firebaseapp.com",
-//   projectId: "ndinterveiw",
-//   storageBucket: "ndinterveiw.appspot.com",
-//   messagingSenderId: "423535912216",
-//   appId: "1:423535912216:web:5665cf6d01e253cb10ea6d",
-//   measurementId: "G-DE2NGHYL6D"
+//   apiKey: "AIzaSyDZxMugqd8B2a53scfhFldyRGrlyPNAvkU",
+//   authDomain: "reels-next-d28a7.firebaseapp.com",
+//   projectId: "reels-next-d28a7",
+//   storageBucket: "reels-next-d28a7.appspot.com",
+//   messagingSenderId: "279478513733",
+//   appId: "1:279478513733:web:05bac33a46cbe29e4bf32c"
 // };
 
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+
+
+
+// const auth = getAuth();
+// const storage = getStorage();
+// const db = getFirestore();
+// export { auth,storage,db }
+// export default app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBN8rNnQ3gfpa1IfeiUrMVC8W_CxV9xOW4",
+  authDomain: "ndinterveiw.firebaseapp.com",
+  projectId: "ndinterveiw",
+  storageBucket: "ndinterveiw.appspot.com",
+  messagingSenderId: "423535912216",
+  appId: "1:423535912216:web:5665cf6d01e253cb10ea6d",
+  measurementId: "G-DE2NGHYL6D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
